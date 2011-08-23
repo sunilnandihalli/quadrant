14ec3245889527f486dc89ce9faa085b7db53cf9
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-23 15:36:13 +0530

    ..first commit

---
+++ src/quadrant/core.clj
@@ -0,0 +1,1 @@
+(ns quadrant.core)
---
+++ test/quadrant/test/core.clj
@@ -0,0 +1,6 @@
+(ns quadrant.test.core
+  (:use [quadrant.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
