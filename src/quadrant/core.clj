26e31c8c9619cb38e5e83c1859a16cabe10245c4
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-27 21:28:58 +0530

    commiting before trying a haskell version

--- src/quadrant/core.clj
+++ src/quadrant/core.clj
@@ -1,23 +1,10 @@
 (ns quadrant.core
+  (:refer-clojure)
   (:gen-class)
-  (:require [clojure.zip :as z])
-  (:use [quadrant.debug]))
-
-(defn update! [trmp k f]
-  (assoc! trmp k (f (trmp k))))
-
-(def _ :undefined)
+  (:require [clojure.zip :as z]))
 
 (defmacro thrush-with-pattern [[pattern] first & exprs]
   (if (seq exprs) `(let [~pattern ~first] (thrush-with-pattern [~pattern] ~@exprs)) first))
-
-(defmacro thrush-with-pattern-dbg [[pattern] first & exprs]
-  (if (seq exprs) `(let [sym# ~first
-                         ~pattern sym#]                     
-                     (println ['~first sym#])
-                     (thrush-with-pattern-dbg [~pattern] ~@exprs))
-      `(let [sym# ~first]
-         (println ['~first sym#]) sym#)))
 
 (defn add-quadrant-counts [a b]
   {:pre [#_(do (println [a b]) true)]}
@@ -26,15 +13,6 @@
                     (first (keep (comp not zero?) (map compare [q1 q2 q3 q4] [q1- q2- q3- q4-]))))
         helper (memoize (fn [a b] (merge-with + a b)))]
     (if (= (mycompare a b) 1) (helper b a) (helper a b))))
-
-(let [counter (atom 0)]
-  (defn show-tree
-    ([loc mp]
-       (let [tr (z/root loc)]
-         (clojure.inspector/inspect-tree  (merge mp {:tree tr}))))
-    ([loc]
-       (show-tree loc {:id @counter})
-       (swap! counter inc))))
 
 (let [quadrant-flipper {'X {:q1 :q4 :q2 :q3 :q3 :q2 :q4 :q1}
                         'Y {:q1 :q2 :q2 :q1 :q3 :q4 :q4 :q3}
@@ -48,7 +26,7 @@
                                     (= f1 identity) f2
                                     (= f2 identity) f1
                                     :else (let [[[x y] :as quadrant-pairs] (map (fn [[k v]] [k (f1 v)]) f2)]
-                                            (if (= x y) (do #_(clojure.pprint/pprint {:msg  "replacing with identity " :vals quadrant-pairs}) identity)
+                                            (if (= x y) identity
                                                 (into {} quadrant-pairs))))) fs))
           helper (fn helper [loc parent-out-of-range-mapper parent-in-range-mapper [s e :as rng]]
                    (let [{:keys [start end mid val child-mapper] :as nd :or {child-mapper identity}} (z/node loc)
@@ -81,7 +59,6 @@
                         [(add-quadrant-counts v-left v-right) ret-loc]))))]
       (helper root-loc identity (quadrant-flipper cmd) [start end]))))
 
-#_(solve)
 
 (defn read-stdin [& {:keys [fname] :or {fname "inp.txt"}}]
   (let [vs (or (seq (doall (line-seq (java.io.BufferedReader. *in*))))
@@ -101,12 +78,6 @@
         [[nq] & queries] (drop (+ n 1) n-q)]
     [tree (into [] (take nq queries))]))
 
-#_(time (def d (read-stdin :fname "inp.txt")))
-#_(time (read-stdin :fname "inp10.txt"))
-#_(time (def tt (read-stdin :fname "inp11.txt")))
-#_(time (def tt (read-stdin :fname "t.txt")))
-#_(read-stdin)
-
 (defn solve []
   (let [[tree queries] (read-stdin :fname "inp100.txt")
         root-loc (z/zipper :mid #(keep identity [(:l %) (:r %)])
@@ -119,41 +90,11 @@
                               x (throw (Exception. "Invalid use of make-node in zipper"))
                               :else nd)) tree)]
     (reduce (fn [loc [id [cmd _ _ :as q]]]
-              #_(show-tree loc {:before-command q :id id})
               (let [[v new-loc] (execute-cmd loc q)]
-                #_(show-tree new-loc {:after-command q :id id})
                 (if (= cmd 'C) (let [{:keys [q1 q2 q3 q4] :or {q1 0 q2 0 q3 0 q4 0}} v]
                                  (println (str q1 " " q2 " " q3 " " q4)))) new-loc))
             root-loc (map-indexed vector queries))))
 
-#_(solve)
-
 (defn -main []
   (solve))
 
-(comment 
-  (def s {:l {:l {:val 4}
-              :r {:val 6}
-              :val 10}
-          :r {:val 20
-              :l {:val 11
-                  :l {:val 3}
-                  :r {:val 8}}
-              :r {:val 9
-                  :l {:val 2}
-                  :r {:val 7}}}
-          :val 30})
-  (def btree (z/zipper identity  #(keep identity [(:l %) (:r %)])
-                       (fn [nd [x y & rest]]
-                         (cond
-                          rest (throw (Exception. "Invalid use of make-node in zipper"))
-                          (and x y) (assoc nd :l x :r y)
-                          x (assoc nd :l x)
-                          :else nd)) s))
-  (def inced-btree 
-    (let [myinc #(when %
-                   (clojure.pprint/pprint (:val %))
-                   (update-in % [:val] inc))]
-      (loop [loc btree]
-        (if (z/end? loc) (z/root loc)
-            (recur (-> (z/edit loc myinc) z/next)))))))
