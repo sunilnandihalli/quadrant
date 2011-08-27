ea6f076a4d6e702642b9a551de69c9b550a0b354
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-27 17:17:28 +0530

    ... seems to work ..

--- src/quadrant/core.clj
+++ src/quadrant/core.clj
@@ -20,69 +20,69 @@
          (println ['~first sym#]) sym#)))
 
 (defn add-quadrant-counts [a b]
+  {:pre [#_(do (println [a b]) true)]}
   (let [mycompare (fn [{:keys [q1 q2 q3 q4] :or {q1 0 q2 0 q3 0 q4 0}}
                        {:keys [q1- q2- q3- q4-] :or {q1- 0 q2- 0 q3- 0 q4- 0}}]
                     (first (keep (comp not zero?) (map compare [q1 q2 q3 q4] [q1- q2- q3- q4-]))))
-        helper (memoize (fn [a b] (if (and a b) (merge-with + a b) (or a b))))]
+        helper (memoize (fn [a b] (merge-with + a b)))]
     (if (= (mycompare a b) 1) (helper b a) (helper a b))))
+
+(let [counter (atom 0)]
+  (defn show-tree
+    ([loc mp]
+       (let [tr (z/root loc)]
+         (clojure.inspector/inspect-tree  (merge mp {:tree tr}))))
+    ([loc]
+       (show-tree loc {:id @counter})
+       (swap! counter inc))))
 
 (let [quadrant-flipper {'X {:q1 :q4 :q2 :q3 :q3 :q2 :q4 :q1}
                         'Y {:q1 :q2 :q2 :q1 :q3 :q4 :q4 :q3}
                         'C identity}
       map-keys (fn map-keys [orig-map old-new-key-map]
-                 (into {} (map (fn [[k v]] [(old-new-key-map k) v]) orig-map)))
-      map-vals (fn map-vals [orig-map old-new-val-map]
-                 (into {} (map (fn [[k v]] [k (old-new-val-map v)]) orig-map)))]
-  (defn execute-cmd [tree [cmd start end]]
-    #_(clojure.inspector/inspect-tree (self-keyed-map tree cmd start end))
-    (let [root-loc (z/zipper identity #(keep identity [(:l %) (:r %)])
-                             (fn [nd [x y & rest]]
-                               (cond
-                                rest (throw (Exception. "Invalid use of make-node in zipper"))
-                                (and x y) (assoc nd :l x :r y)
-                                x (assoc nd :l x)
-                                :else nd)) tree)
-          helper (fn helper [loc parent-whole-modifier parent-part-modifier s e]
-                    (let [{:keys [start end mid val child-modifier]} (z/node loc)
-                          f-new-whole (if child-modifier (comp parent-whole-modifier child-modifier) parent-whole-modifier)
-                          f-new-part (if child-modifier (comp parent-part-modifier child-modifier) parent-part-modifier)]
-                      (cond
-                       (and (nil? s) (nil? e)) [nil (z/edit loc (fn [nd] (-> (update-in nd [:val] #(map-keys % parent-whole-modifier))
-                                                                             (assoc :child-modifiers f-new-whole))))]
-                       (and (= s start) (= e end)) (let [ret-loc (z/edit loc (fn [nd] (-> (update-in nd [:val] #(map-keys % parent-part-modifier))
-                                                                                          (assoc :child-modifiers f-new-part))))]
-                                                     [(:val (z/node ret-loc)) ret-loc])
-                       (< s mid e) (let [loc-left-initial (z/down loc)
-                                         [v-left loc-left-final] (helper loc-left-initial f-new-whole f-new-part s (int mid))
-                                         loc-right-initial (z/right loc-left-final)
-                                         [v-right loc-right-final] (helper loc-right-initial f-new-whole f-new-part (inc (int mid)) e)
-                                         cur-loc (z/up loc-right-final)
-                                         ret-loc (z/edit cur-loc (fn [nd] (-> (dissoc nd :child-modifiers)
-                                                                              (assoc :val (add-quadrant-counts (:val (z/node loc-left-final))
-                                                                                                               (:val (z/node loc-right-final)))))))]
-                                     [(add-quadrant-counts v-left v-right) ret-loc])
-                       (< e mid) (let [loc-left-initial (z/down loc)
-                                       [v-left loc-left-final] (helper loc-left-initial f-new-whole f-new-part s e)
-                                       loc-right-initial (z/right loc-left-final)
-                                       [_ loc-right-final] (helper loc-left-final f-new-whole identity nil nil)
-                                       cur-loc (z/up loc-right-final)
-                                       ret-loc (z/edit cur-loc (fn [nd] (-> (dissoc nd :child-modifiers)
-                                                                            (assoc :val (add-quadrant-counts (:val (z/node loc-left-final))
-                                                                                                             (:val (z/node loc-right-final)))))))]
-                                   [v-left ret-loc])
-                       (< mid s) (let [loc-left-initial (z/down loc)
-                                       [_ loc-left-final] (helper loc-left-initial f-new-whole identity nil nil)
-                                       loc-right-initial (z/right loc-left-final)
-                                       [v-right loc-right-final] (helper loc-right-initial f-new-whole f-new-part s e)
-                                       cur-loc (z/up loc-right-final)
-                                       ret-loc (z/edit cur-loc (fn [nd] (-> (dissoc nd :child-modifiers)
-                                                                            (assoc :val (add-quadrant-counts (:val (z/node loc-left-final))
-                                                                                                             (:val (z/node loc-right-final)))))))]
-                                   [v-right ret-loc]))))
-          [v loc] (helper root-loc identity (quadrant-flipper cmd) start end)]
-      [v (z/root loc)])))
+                 (into {} (map (fn [[k v]] [(old-new-key-map k) v]) orig-map)))]
+  (defn execute-cmd [root-loc [cmd start end]]
+    (let [map-composer (fn [& fs]
+                         (reduce (fn [f1 f2]
+                                   (cond
+                                    (= f1 identity) f2
+                                    (= f2 identity) f1
+                                    :else (let [[[x y] :as quadrant-pairs] (map (fn [[k v]] [k (f1 v)]) f2)]
+                                            (if (= x y) (do #_(clojure.pprint/pprint {:msg  "replacing with identity " :vals quadrant-pairs}) identity)
+                                                (into {} quadrant-pairs))))) fs))
+          helper (fn helper [loc parent-out-of-range-mapper parent-in-range-mapper [s e :as rng]]
+                   (let [{:keys [start end mid val child-mapper] :as nd :or {child-mapper identity}} (z/node loc)
+                         out-of-range-mapper (map-composer parent-out-of-range-mapper child-mapper)
+                         in-range-mapper (map-composer parent-in-range-mapper child-mapper)]
+                     (cond
+                      (nil? rng) [{} (z/edit loc (fn [nd] (thrush-with-pattern [x]
+                                                            (update-in nd [:val] #(map-keys % parent-out-of-range-mapper))
+                                                            (if mid (if (not= identity out-of-range-mapper)
+                                                                      (assoc x :child-mapper out-of-range-mapper)
+                                                                      (dissoc x :child-mapper)) x))))]
+                      (and (= s start) (= e end)) (let [ret-loc (z/edit loc (fn [nd]
+                                                                              (thrush-with-pattern [x]
+                                                                                (update-in nd [:val] #(map-keys % parent-in-range-mapper))
+                                                                                (if mid (if (not= identity in-range-mapper)
+                                                                                          (assoc x :child-mapper in-range-mapper)
+                                                                                          (dissoc x :child-mapper)) x))))]
+                                                    [(:val (z/node ret-loc)) ret-loc])
+                      :else
+                      (let [case-type (cond (< s mid e) :s-m-e (< e mid) :s-e-m (< mid s) :m-s-e)
+                            loc-left-initial (z/down loc)
+                            m (int mid)
+                            [v-left loc-left-final] (helper loc-left-initial out-of-range-mapper in-range-mapper (case case-type :s-m-e [s m] :s-e-m [s e] :m-s-e nil))
+                            loc-right-initial (z/right loc-left-final)
+                            [v-right loc-right-final] (helper loc-right-initial out-of-range-mapper in-range-mapper (case case-type :s-m-e [(inc m) e] :s-e-m nil :m-s-e [s e]))
+                            cur-loc (z/up loc-right-final)
+                            ret-loc (z/edit cur-loc (fn [nd] (-> (dissoc nd :child-mapper)
+                                                                 (assoc :val (add-quadrant-counts (:val (z/node loc-left-final))
+                                                                                                  (:val (z/node loc-right-final)))))))]
+                        [(add-quadrant-counts v-left v-right) ret-loc]))))]
+      (helper root-loc identity (quadrant-flipper cmd) [start end]))))
 
 #_(solve)
+
 (defn read-stdin [& {:keys [fname] :or {fname "inp.txt"}}]
   (let [vs (or (seq (doall (line-seq (java.io.BufferedReader. *in*))))
                (seq (doall (line-seq
@@ -108,12 +108,26 @@
 #_(read-stdin)
 
 (defn solve []
-  (let [[tree queries] (read-stdin :fname "l.2.txt")]
-    (reduce (fn [ct [cmd _ _ :as q]]
-              (let [[v nt] (execute-cmd ct q)]
+  (let [[tree queries] (read-stdin :fname "inp100.txt")
+        root-loc (z/zipper :mid #(keep identity [(:l %) (:r %)])
+                           (fn [nd [x y & rest]]
+                             #_(println "calling make-node")
+                             #_(clojure.pprint/pprint (self-keyed-map nd x y))
+                             (cond
+                              rest (throw (Exception. "Invalid use of make-node in zipper"))
+                              (and x y) (assoc nd :l x :r y :val (add-quadrant-counts (:val x) (:val y)))
+                              x (throw (Exception. "Invalid use of make-node in zipper"))
+                              :else nd)) tree)]
+    (reduce (fn [loc [id [cmd _ _ :as q]]]
+              #_(show-tree loc {:before-command q :id id})
+              (let [[v new-loc] (execute-cmd loc q)]
+                #_(show-tree new-loc {:after-command q :id id})
                 (if (= cmd 'C) (let [{:keys [q1 q2 q3 q4] :or {q1 0 q2 0 q3 0 q4 0}} v]
-                                 (println (str q1 " " q2 " " q3 " " q4)))) nt)) tree queries)))
+                                 (println (str q1 " " q2 " " q3 " " q4)))) new-loc))
+            root-loc (map-indexed vector queries))))
+
 #_(solve)
+
 (defn -main []
   (solve))
 
